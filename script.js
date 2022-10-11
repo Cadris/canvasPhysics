9c3510677a2e0b6b02330228b174d7c96e9d2c88
Cadris <cadrissilver@gmail.com>
2022-10-12 02:03:10 +0530

    abstaract done

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ window.addEventListener('load', function(){
      * Handles individual particles
      */
     class Particle{
-        constructor(effect){
+        constructor(effect, x, y, color){
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
             this.y = Math.random() * this.effect.height;
             this.size = Math.random() * 10;
+
+            this.originX = Math.floor(x);
+            this.originY = Math.floor(y);
+            this.color = color;
 
             // velocity x,y
             this.vx = Math.random() * 2 - 1 ;
@@ -46,17 +50,33 @@ window.addEventListener('load', function(){
 
             this.x = this.centerX - this.image.width * 0.5;
             this.y = this.centerY - this.image.height * 0.5;
+
+            this.gap = 5;
         }
         
-        init(){
-            for (let i = 0; i < 100; i++) {
-                this.particleArray.push(new Particle(this));
+        init(context){
+            context.drawImage(this.image, this.x, this.y);
+            const pixels = context.getImageData(0, 0, this.width, this.height).data;
+            for(let y = 0; y<this.height; y+=this.gap){
+                for(let x = 0; x<this.width; x+=this.gap){
+                    const index = (y * this.width +x) * 4;
+                    const red = pixels[index];
+                    const green = pixels[index + 1];
+                    const blue = pixels[index + 2];
+                    const alpha = pixels[index + 3];
+
+                    const color = `rgb(${red}, ${green}, ${blue})`;
+
+                    if(alpha > 0){
+                        this.particleArray.push(new Particle(this, x, y, color));
+                    }
+
+                }
             }
         }
 
         draw(context){
             this.particleArray.forEach(particle => particle.draw(context));
-            context.drawImage(this.image, this.x, this.y);
         }
 
         update(){
@@ -65,7 +85,7 @@ window.addEventListener('load', function(){
     }
 
     const effect = new Effect(canvas.width, canvas.height);
-    effect.init();
+    effect.init(ctx);
 
     /**
      * Animation loop
@@ -77,7 +97,7 @@ window.addEventListener('load', function(){
         window.requestAnimationFrame(animate);
     }
 
-    animate();
+    //animate();
 
     /**
      * draw on canvas - x, y, width, height
